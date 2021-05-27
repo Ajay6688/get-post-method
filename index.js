789c18b0cf600e01cf3021cd567ab44af54f4a98
Ajay <ajnabha6688@gmail.com>
2021-05-27 18:27:46 +0530

    get post put delete req

--- index.js
+++ index.js
@@ -7,20 +7,54 @@ let data = JSON.parse(fs.readFileSync("data.json", "utf8"));
 
 app.use(Express.json());
 
+function updateData() {
+    fs.writeFile('data.json', JSON.stringify(data), () => {
+        console.log("data updated successfully");
+    });
+}
+
 app.get('/', (req, res) => {
-    res.send("hello");
+    res.send("This is home page")
+});
+app.get('/api/data', (req, res) => {
+    res.send(data);
 });
 
-app.post('/', (req, res) => {
+app.post('api/post', (req, res) => {
     let postData = req.body;
-    // console.log(typeof postData);
+
     data.push(postData);
     // Writing to our JSON file
-    fs.writeFile("data.json", JSON.stringify(data), () => {
-        console.log("New data added");
-    });
+    // fs.writeFile("data.json", JSON.stringify(data), () => {
+    //     console.log("New data added");
+    // });
     console.log(data);
     res.send("post method called");
 });
 
+app.put('/api/update/:id', (req, res) => {
+    const id = req.params.id;
+    let name = req.body.name;
+    let group = req.body.group;
+    const index = data.findIndex((user) => {
+        return (user.id == id);
+    });
+    data[index].id = id;
+    data[index].name = name;
+    data[index].group = group;
+    // console.log(data[index]);
+    console.log(data);
+    updateData();
+
+});
+
+app.delete('/api/delete/:id', (req, res) => {
+    let id = req.params.id;
+    let index = data.findIndex((user) => {
+        return (user.id == id);
+    });
+    data.splice(index, 1);
+    updateData();
+    console.log(data);
+});
 app.listen(port, () => console.log('server has been started at port ' + port));
